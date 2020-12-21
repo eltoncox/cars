8fe5b9b221bc44023ab55f74a3f19e07ae8e700d
eltoncox <elton.unix@gmail.com>
2020-12-21 15:02:40 -0300

    enviando projeto para o git

---
+++ src/main/java/com/carros/domain/UserRepository.java
@@ -0,0 +1,11 @@
+package com.carros.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserByLogin(String login);
+
+    User findByLogin(String username);
+}
